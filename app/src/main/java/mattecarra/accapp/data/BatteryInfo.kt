08eb622268974c6dd6f922f016eacebc2569a9f2
Matteo Carrara <matte.carra@gmail.com>
2019-02-20 11:24:05 +0100

    Added support for mV or uV VOLTAGE_NOW

--- app/src/main/java/mattecarra/accapp/data/BatteryInfo.kt
+++ app/src/main/java/mattecarra/accapp/data/BatteryInfo.kt
@@ -1,3 +1,9 @@
 package mattecarra.accapp.data
 
-class BatteryInfo(val status: String, val health: String, val current: Int, val voltage: Int, val temp: Int)
+class BatteryInfo(val status: String, val health: String, val current: Int, voltage: Int, val temp: Int) {
+    val voltage: Float =
+        if(voltage > 1000000)
+            voltage / 1000000f
+        else
+            voltage / 1000f
+}
