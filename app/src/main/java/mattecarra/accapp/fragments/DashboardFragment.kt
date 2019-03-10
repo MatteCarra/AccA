5197769f41da42f4b76be3391ca0f872208b205a
Jayden <squabbi@users.noreply.github.com>
2019-03-10 22:02:23 +1100

    Added Dashboard View Fragment

---
+++ app/src/main/java/mattecarra/accapp/fragments/DashboardFragment.kt
@@ -0,0 +1,33 @@
+package mattecarra.accapp.fragments
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import mattecarra.accapp.R
+
+class DashboardFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = DashboardFragment()
+    }
+
+    private lateinit var viewModel: DashboardViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.dashboard_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(DashboardViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/mattecarra/accapp/fragments/DashboardViewModel.kt
@@ -0,0 +1,7 @@
+package mattecarra.accapp.fragments
+
+import androidx.lifecycle.ViewModel;
+
+class DashboardViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
