71a1bac84acb84b844f8075358d69d57ac0eb805
squabbi <squabbi@users.noreply.github.com>
2019-03-18 19:43:19 +1100

    Added SchedulesFragment and ViewModel

---
+++ app/src/main/java/mattecarra/accapp/fragments/SchedulesFragment.kt
@@ -0,0 +1,33 @@
+package mattecarra.accapp.fragments
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import mattecarra.accapp.R
+
+class SchedulesFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = SchedulesFragment()
+    }
+
+    private lateinit var viewModel: SchedulesViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.schedules_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(SchedulesViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
