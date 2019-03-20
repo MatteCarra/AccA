1820a4e29e1cfc29215b03f6ef6d7432b6ef6749
squabbi <squabbi@users.noreply.github.com>
2019-03-20 18:10:07 +1100

    Created data class for profile

--- app/src/main/java/mattecarra/accapp/models/Profile.kt
+++ app/src/main/java/mattecarra/accapp/models/Profile.kt
@@ -1,4 +1,25 @@
 package mattecarra.accapp.models
 
-class Profile {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "profiles_table")
+data class Profile(
+    @PrimaryKey(autoGenerate = true) val uid: Int,
+    var cooldownCapacity: Int,
+    var pauseCapacity: Int,
+    var resumeCapacity: Int,
+    var shutdownCapacity: Int,
+    var chargingSwitch: String?,
+    var cooldownCharge: Int,
+    var cooldownPause: Int,
+    var onBootCommand: String?,
+    var onBootExit: Boolean,
+    var onPluggedCommand: String?,
+    var resetUnplugged: Boolean,
+    var cooldownTemperature: Int,
+    var pauseChargingTemperature: Int,
+    var cooldownWaitTime: Int,
+    var voltageControlFile: String?,
+    var voltageMax: Int?
+)
