c3b26dafcfc22a12c0765f335f9f900d1c010719
squabbi <squabbi@users.noreply.github.com>
2019-03-20 18:12:10 +1100

    Initalised database with profiles

--- app/src/main/java/mattecarra/accapp/database/AccaRoomDatabase.kt
+++ app/src/main/java/mattecarra/accapp/database/AccaRoomDatabase.kt
@@ -2,18 +2,33 @@ package mattecarra.accapp.database
 
 import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import mattecarra.accapp.models.Profile
 
 @Database(entities = [Profile::class], version = 1)
-public abstract class RoomAccaDatabase : RoomDatabase() {
+public abstract class AccaRoomDatabase : RoomDatabase()
 
-    companion object {
-        @Volatile
-        private var INSTANCE: RoomAccaDatabase? = null
+val DATABASE_NAME = "acca_database"
 
-        fun getDatabase(context: Context): 
+companion object {
+    @Volatile
+    private var INSTANCE: AccaRoomDatabase? = null
+
+    fun getDatabase(context: Context): AccaRoomDatabase {
+
+        return INSTANCE ?: synchronized(this) {
+            // Create database instance here
+            val instance = Room.databaseBuilder(
+                context.applicationContext,
+                AccaRoomDatabase::class.java,
+                DATABASE_NAME
+            ).build()
+
+            
+        }
     }
+}
 
-    abstract fun profileDao(): ProfileDao
+abstract fun profileDao(): ProfileDao
 }
