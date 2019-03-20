9eb6e6179a40c420c6f7d12b0168819a7e2418d4
squabbi <squabbi@users.noreply.github.com>
2019-03-20 18:11:57 +1100

    Constructed basic profiles DAO

--- app/src/main/java/mattecarra/accapp/database/ProfileDao.kt
+++ app/src/main/java/mattecarra/accapp/database/ProfileDao.kt
@@ -1,6 +1,20 @@
 package mattecarra.accapp.database
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import mattecarra.accapp.models.Profile
 
 @Dao
-interface ProfilesDao
+interface ProfileDao {
+
+    @Query("SELECT * FROM profiles_table ORDER BY uid DESC")
+    fun getAllProfiles(): LiveData<List<Profile>>
+
+    @Insert
+    fun insert(profile: Profile)
+
+    @Query("DELETE FROM profiles_table")
+    fun deleteAll()
+}
