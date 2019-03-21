d05b6b5d4179616a70c5518cd8d9627ae2400a8f
squabbi <squabbi@users.noreply.github.com>
2019-03-21 12:11:20 +1100

    Implemented basic database

--- app/src/main/java/mattecarra/accapp/database/AccaRoomDatabase.kt
+++ app/src/main/java/mattecarra/accapp/database/AccaRoomDatabase.kt
@@ -4,31 +4,38 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import mattecarra.accapp.models.Profile
+import mattecarra.accapp.models.ProfileEntity
 
-@Database(entities = [Profile::class], version = 1)
-public abstract class AccaRoomDatabase : RoomDatabase()
+@Database(entities = [ProfileEntity::class], version = 1)
+abstract class AccaRoomDatabase : RoomDatabase() {
 
-val DATABASE_NAME = "acca_database"
+    abstract fun profileDao(): ProfileDao
 
-companion object {
-    @Volatile
-    private var INSTANCE: AccaRoomDatabase? = null
+    companion object {
+        @Volatile
+        private var INSTANCE: AccaRoomDatabase? = null
 
-    fun getDatabase(context: Context): AccaRoomDatabase {
+        val DATABASE_NAME = "acca_database"
 
-        return INSTANCE ?: synchronized(this) {
-            // Create database instance here
-            val instance = Room.databaseBuilder(
-                context.applicationContext,
-                AccaRoomDatabase::class.java,
-                DATABASE_NAME
-            ).build()
+        fun getDatabase(context: Context): AccaRoomDatabase {
 
-            
+            val tempInstance = INSTANCE
+            if (tempInstance != null) {
+                return tempInstance
+            }
+
+            synchronized(this) {
+                // Create database instance here
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AccaRoomDatabase::class.java,
+                    DATABASE_NAME
+                ).build()
+
+                INSTANCE = instance
+                return instance
+            }
         }
     }
-}
 
-abstract fun profileDao(): ProfileDao
 }
