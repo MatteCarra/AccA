80daec8a849991cfa252dbe901cbebd47558f7a3
squabbi <squabbi@users.noreply.github.com>
2019-03-21 12:12:56 +1100

    WIP data repository

--- app/src/main/java/mattecarra/accapp/utils/DataRepository.kt
+++ app/src/main/java/mattecarra/accapp/utils/DataRepository.kt
@@ -1,4 +1,15 @@
 package mattecarra.accapp.utils
 
-class DataRepository {
+import androidx.lifecycle.LiveData
+import mattecarra.accapp.database.AccaRoomDatabase
+import mattecarra.accapp.models.ProfileEntity
+
+class DataRepository constructor(private val accaRoomDatabase: AccaRoomDatabase) {
+
+    private var sInstance: DataRepository
+    private val mAccaDatabase: AccaRoomDatabase
+
+    private val mObservableProfiles: LiveData<List<ProfileEntity>>
+
+    fun get
 }
