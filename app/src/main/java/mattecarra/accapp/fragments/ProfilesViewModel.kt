03f52489380468b03578731db2c22d2e3ce19767
squabbi <squabbi@users.noreply.github.com>
2019-03-21 14:20:46 +1100

    Implemented basic ViewModel with application context to access database functions and data from the repository.

--- app/src/main/java/mattecarra/accapp/fragments/ProfilesViewModel.kt
+++ app/src/main/java/mattecarra/accapp/fragments/ProfilesViewModel.kt
@@ -1,7 +1,40 @@
 package mattecarra.accapp.fragments
 
-import androidx.lifecycle.ViewModel;
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
 
-class ProfilesViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+import mattecarra.accapp.models.ProfileEntity
+import mattecarra.accapp.utils.DataRepository
+
+class ProfilesViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val mDataRepository: DataRepository
+
+    private val mProfilesListLiveData: LiveData<List<ProfileEntity>>
+
+    private var mParentJob = Job()
+    private val mCoroutineContext: CoroutineContext
+        get() = mParentJob + Dispatchers.Main
+
+    private val mScope = CoroutineScope(mCoroutineContext)
+
+    init {
+
+        mDataRepository = DataRepository(application)
+        mProfilesListLiveData = mDataRepository.getAllProfiles()
+    }
+
+    fun insertProfile(profile: ProfileEntity) = mScope.launch(Dispatchers.IO) {
+
+        mDataRepository.insertProfile(profile)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        mParentJob.cancel()
+    }
 }
