869fabb118023f24ac5ac18b041c9195231bd800
squabbi <squabbi@users.noreply.github.com>
2019-03-21 12:12:21 +1100

    Added in CRUD functions

--- app/src/main/java/mattecarra/accapp/database/ProfileDao.kt
+++ app/src/main/java/mattecarra/accapp/database/ProfileDao.kt
@@ -1,19 +1,26 @@
 package mattecarra.accapp.database
 
 import androidx.lifecycle.LiveData
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.Query
-import mattecarra.accapp.models.Profile
+import androidx.room.*
+import mattecarra.accapp.models.ProfileEntity
 
 @Dao
 interface ProfileDao {
 
     @Query("SELECT * FROM profiles_table ORDER BY uid DESC")
-    fun getAllProfiles(): LiveData<List<Profile>>
+    fun getAllProfiles(): LiveData<List<ProfileEntity>>
 
     @Insert
-    fun insert(profile: Profile)
+    fun insert(profileEntity: ProfileEntity)
+
+    @Update
+    fun update(profileEntity: ProfileEntity)
+
+    @Delete
+    fun delete(profileEntity: ProfileEntity)
+
+    @Query("SELECT * FROM profiles_table WHERE profileName == :name")
+    fun getProfileByName(name: String): List<ProfileEntity>
 
     @Query("DELETE FROM profiles_table")
     fun deleteAll()
