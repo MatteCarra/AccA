abae0e1b6d8b3892a97dd13b2867570c260d5c56
Squabbi <squabbi@users.noreply.github.com>
2019-03-29 10:20:53 +1100

    Added delete profile function

--- app/src/main/java/mattecarra/accapp/activities/MainActivityViewModel.kt
+++ app/src/main/java/mattecarra/accapp/activities/MainActivityViewModel.kt
@@ -34,6 +34,10 @@ class MainActivityViewModel(application: Application) : AndroidViewModel(applica
         mDataRepository.insertProfile(profile)
     }
 
+    fun deleteProfile(profile: AccaProfile) = mScope.launch(Dispatchers.IO) {
+        mDataRepository.deleteProfile(profile)
+    }
+
     /**
      * Clears the currently selected profile ID from Shared Preferences.
      */
