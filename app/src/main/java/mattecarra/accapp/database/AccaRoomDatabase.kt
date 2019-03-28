f183ac8deba129b6cb6485cddba0cd4f5ce3168a
Squabbi <squabbi@users.noreply.github.com>
2019-03-28 17:04:48 +1100

    Added TypeConverter

--- app/src/main/java/mattecarra/accapp/database/AccaRoomDatabase.kt
+++ app/src/main/java/mattecarra/accapp/database/AccaRoomDatabase.kt
@@ -4,10 +4,12 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import kotlinx.coroutines.CoroutineScope
 import mattecarra.accapp.models.AccaProfile
 
 @Database(entities = [AccaProfile::class], version = 1)
+@TypeConverters(ConfigConverter::class)
 abstract class AccaRoomDatabase : RoomDatabase() {
 
     abstract fun profileDao(): ProfileDao
