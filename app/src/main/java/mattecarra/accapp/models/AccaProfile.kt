7f1fe63b6577d4770620932a30733eef812f74da
squabbi <squabbi@users.noreply.github.com>
2019-03-28 11:52:20 +1100

    Removed duplicated vars from AccConfig object

--- app/src/main/java/mattecarra/accapp/models/AccaProfile.kt
+++ app/src/main/java/mattecarra/accapp/models/AccaProfile.kt
@@ -10,20 +10,4 @@ data class AccaProfile(
     @PrimaryKey(autoGenerate = true) val uid: Int,
     var profileName: String,
     @Embedded var accConfig: AccConfig
-//    var cooldownCapacity: Int,
-//    var pauseCapacity: Int,
-//    var resumeCapacity: Int,
-//    var shutdownCapacity: Int,
-//    var chargingSwitch: String?,
-//    var cooldownCharge: Int,
-//    var cooldownPause: Int,
-//    var onBootCommand: String?,
-//    var onBootExit: Boolean,
-//    var onPluggedCommand: String?,
-//    var resetUnplugged: Boolean,
-//    var cooldownTemperature: Int,
-//    var pauseChargingTemperature: Int,
-//    var cooldownWaitTime: Int,
-//    var voltageControlFile: String?,
-//    var voltageMax: Int?
 )
