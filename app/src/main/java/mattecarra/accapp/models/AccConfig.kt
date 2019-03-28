61d2c3f733aeb0dc66d3f2b9490336bb6ab0e969
squabbi <squabbi@users.noreply.github.com>
2019-03-28 11:56:18 +1100

    Added config data variables

--- app/src/main/java/mattecarra/accapp/models/AccConfig.kt
+++ app/src/main/java/mattecarra/accapp/models/AccConfig.kt
@@ -1,4 +1,20 @@
 package mattecarra.accapp.models
 
-class AccConfig {
-}
+data class AccConfig (
+    var cooldownCapacity: Int,
+    var pauseCapacity: Int,
+    var resumeCapacity: Int,
+    var shutdownCapacity: Int,
+    var chargingSwitch: String?,
+    var cooldownCharge: Int,
+    var cooldownPause: Int,
+    var onBootCommand: String?,
+    var onBootExit: Boolean,
+    var onPluggedCommand: String?,
+    var resetUnplugged: Boolean,
+    var cooldownTemperature: Int,
+    var pauseChargingTemperature: Int,
+    var cooldownWaitTime: Int,
+    var voltageControlFile: String?,
+    var voltageMax: Int?
+)
