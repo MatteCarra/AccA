d9cb6519f1915a95df669df77584a947b3811915
Squabbi <squabbi@users.noreply.github.com>
2019-05-15 22:49:48 +1000

    Added required imports

--- app/src/main/java/mattecarra/accapp/utils/ProfileUtils.kt
+++ app/src/main/java/mattecarra/accapp/utils/ProfileUtils.kt
@@ -3,9 +3,12 @@ package mattecarra.accapp.utils
 import android.content.Context
 import android.content.SharedPreferences
 import com.google.gson.Gson
+import mattecarra.accapp.database.AccaRoomDatabase
+import mattecarra.accapp.models.AccaProfile
 import java.io.File
 
 object ProfileUtils {
+
     fun getCurrentProfile(sharedPrefs: SharedPreferences): String? {
         return sharedPrefs.getString("PROFILE", null)
     }
