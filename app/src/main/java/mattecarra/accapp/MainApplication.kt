2020a0701540b9694ac84e6b3af06dadc665b3b9
Squabbi <squabbi@users.noreply.github.com>
2019-06-22 20:38:05 +1000

    Context access for non activities/fragments

--- app/src/main/java/mattecarra/accapp/MainApplication.kt
+++ app/src/main/java/mattecarra/accapp/MainApplication.kt
@@ -1,10 +1,19 @@
 package mattecarra.accapp
 
 import android.app.Application
+import android.content.Context
 import com.topjohnwu.superuser.Shell
 
 class MainApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        context = this
+    }
+
     companion object {
+        lateinit var context:Context
+
         init {
             Shell.Config.setFlags(Shell.FLAG_REDIRECT_STDERR)
             Shell.Config.verboseLogging(false /*BuildConfig.DEBUG*/)
