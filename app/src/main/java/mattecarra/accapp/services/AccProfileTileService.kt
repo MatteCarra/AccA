b1188172514928028f045062ab687766d414c64f
Matteo Carrara <matte.carra@gmail.com>
2019-06-29 14:54:51 +0200

    Added AccProfileTileService back

--- app/src/main/java/mattecarra/accapp/services/AccProfileTileService.kt
+++ app/src/main/java/mattecarra/accapp/services/AccProfileTileService.kt
@@ -11,38 +11,52 @@ import android.service.quicksettings.Tile
 import android.service.quicksettings.TileService
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.Observer
 import com.google.gson.Gson
 import mattecarra.accapp.R
+import mattecarra.accapp.acc.Acc
+import mattecarra.accapp.fragments.ProfilesViewModel
 import mattecarra.accapp.utils.ProfileUtils
 import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
 import java.io.File
 
 @TargetApi(Build.VERSION_CODES.N)
 class AccProfileTileService: TileService() {
     private val LOG_TAG = "AccProfileTileService"
+    private lateinit var profilesViewModel: ProfilesViewModel
+
+    override fun onCreate() {
+        super.onCreate()
+
+        profilesViewModel = ProfilesViewModel(application)
+        profilesViewModel.getProfiles().observeForever(Observer {
+            updateTile()
+        })
+    }
 
     private fun updateTile() {
         val tile = qsTile
 
-        //TODO check if there is any profile
-        /*if(isThereAnyProfile()) {
-            tile.label = getString(R.string.no_profiles)
-            tile.state =  Tile.STATE_UNAVAILABLE
-            tile.icon = Icon.createWithResource(this, R.drawable.ic_battery_charging_full) //use acc icon once ready
-        } else {
+        val profiles = profilesViewModel.getProfiles().value
+        if(profiles?.isNotEmpty() == true) {
             val profileId = ProfileUtils.getCurrentProfile(PreferenceManager.getDefaultSharedPreferences(this))
-            if(profileId != -1) {
-                tile.label =  getString(R.string.profile_tile_label, profileId.toString()) //TODO get profile name from profileId
+            val currProfile = if(profileId != -1) profiles.find { it.uid == profileId } else null
+            if(currProfile != null) {
+                tile.label =  getString(R.string.profile_tile_label, currProfile.profileName)
                 tile.state =  Tile.STATE_ACTIVE
             } else {
                 tile.label = getString(R.string.profile_not_selected)
                 tile.state =  Tile.STATE_INACTIVE
             }
             tile.icon = Icon.createWithResource(this, R.drawable.ic_battery_charging_80) //use acc icon once ready
-        }*/
+        } else {
+            tile.label = getString(R.string.no_profiles)
+            tile.state =  Tile.STATE_UNAVAILABLE
+            tile.icon = Icon.createWithResource(this, R.drawable.ic_battery_charging_full) //use acc icon once ready
+        }
 
         tile.updateTile()
-
     }
 
     override fun onTileAdded() {
@@ -55,39 +69,45 @@ class AccProfileTileService: TileService() {
         updateTile()
     }
 
+    //Get profiles list and increment current profile of one unit.
     override fun onClick() {
         super.onClick()
-        //TODO: Implement profile incrementer
-        //Get profiles list and increment current profile of one unit.
 
-        /* TODO: Adjust profile application function
+        val sharedPrefs: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
 
-            val gson = Gson()
-            val sharedPrefs: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
-
-            val profileList = ProfileUtils.listProfiles(this, gson)
-
+        profilesViewModel.getProfiles().value?.let { profileList ->
             val currentProfile = ProfileUtils.getCurrentProfile(sharedPrefs)
 
-            var index = (currentProfile?.let { profileList.indexOf(it) } ?: -1) + 1
+            var index = profileList.indexOfFirst { it.uid ==  currentProfile} + 1
             if(index >= profileList.size)
                 index = 0
 
-            val profileToApply = profileList[index]
+            val profile = profileList[index]
 
             //apply profile
-            val profileConfig = ProfileUtils.readProfile(profileToApply, this, gson)
-
             doAsync {
-                profileConfig.updateAcc()
+                val res = Acc.instance.updateAccConfig(profile.accConfig)
 
-                ProfileUtils.saveCurrentProfile(profileToApply, sharedPrefs)
+                if(!res.isSuccessful()) {
+                    res.debug()
+
+                    uiThread {
+                        //Update tile infos
+                        qsTile.state =  Tile.STATE_ACTIVE
+                        qsTile.label =  getString(R.string.error_occurred)
+                        qsTile.updateTile()
+                    }
+                } else {
+                    uiThread {
+                        //Update tile infos
+                        qsTile.state =  Tile.STATE_ACTIVE
+                        qsTile.label =  getString(R.string.profile_tile_label, profile.profileName)
+                        qsTile.updateTile()
+                    }
+                }
+
+                ProfileUtils.saveCurrentProfile(profile.uid, sharedPrefs)
             }
-
-            //Update tile infos
-            qsTile.state =  Tile.STATE_ACTIVE
-            qsTile.label =  getString(R.string.profile_tile_label, profileToApply)
-            qsTile.updateTile()
-        */
+        }
     }
 }
