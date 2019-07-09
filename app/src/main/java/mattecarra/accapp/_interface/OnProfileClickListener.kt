c7da04cae30a0ff91c1b96b5d08f0e35ece9e3f1
Squabbi <squabbi@users.noreply.github.com>
2019-07-09 09:38:55 +0800

    Added profiles options on click listener

--- app/src/main/java/mattecarra/accapp/_interface/OnProfileClickListener.kt
+++ app/src/main/java/mattecarra/accapp/_interface/OnProfileClickListener.kt
@@ -9,4 +9,5 @@ interface OnProfileClickListener {
 
     fun onProfileClick(accaProfile: AccaProfile)
     fun onProfileLongClick(accaProfile: AccaProfile)
+    fun onProfileOptionsClick(accaProfile: AccaProfile)
 }
