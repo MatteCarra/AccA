9cb87fa9550bb14b686ace7a46ab67d25de99911
Matteo Carrara <matte.carra@gmail.com>
2019-07-10 15:04:41 +0200

    Removed permissions to access external storage

--- app/src/main/java/mattecarra/accapp/activities/MainActivity.kt
+++ app/src/main/java/mattecarra/accapp/activities/MainActivity.kt
@@ -46,7 +46,6 @@ import org.jetbrains.anko.uiThread
 class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener, OnProfileClickListener {
 
     private val LOG_TAG = "MainActivity"
-    private val PERMISSION_REQUEST = 0
     private val ACC_CONFIG_EDITOR_REQUEST = 1
     private val ACC_PROFILE_CREATOR_REQUEST = 2
     private val ACC_PROFILE_EDITOR_REQUEST = 3
@@ -111,24 +110,6 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
             title(R.string.config_error_title)
             message(R.string.config_error_dialog)
             positiveButton(android.R.string.ok)
-        }
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
-        when (requestCode) {
-            PERMISSION_REQUEST -> {
-                // If request is cancelled, the result arrays are empty.
-                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
-                    initUi()
-                } else {
-                    finish()
-                }
-                return
-            }
-
-            else -> {
-                // Ignore all other requests.
-            }
         }
     }
 
@@ -372,14 +353,6 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
 //        }
     }
 
-    private fun checkPermissions(): Boolean {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE), PERMISSION_REQUEST)
-            return false
-        }
-        return true
-    }
-
     private fun checkAccInstalled(): Boolean {
         if(!Acc.isBundledAccInstalled(this)) { //TODO let the user decide between bundle and stable
             val dialog = MaterialDialog(this).show {
@@ -404,7 +377,7 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
                                 title(R.string.installation_failed_title)
                                 message(R.string.installation_failed)
                                 positiveButton(R.string.retry) {
-                                    if(checkAccInstalled() && checkPermissions())
+                                    if(checkAccInstalled())
                                         initUi()
                                 }
                                 negativeButton {
@@ -421,8 +394,7 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
                             } else true
                         }
                     } else {
-                        if(checkPermissions())
-                            initUi()
+                        initUi()
                     }
                 }
 
@@ -470,7 +442,7 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
             return
         }
 
-        if(checkAccInstalled() && checkPermissions()) {
+        if(checkAccInstalled()) {
             initUi()
         }
     }
