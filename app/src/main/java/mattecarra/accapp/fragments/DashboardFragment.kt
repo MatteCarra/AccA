ecd43c7edea1f80bc0165b48e56c89e6e5f8462c
Squabbi <squabbi@users.noreply.github.com>
2019-08-17 17:39:37 +1000

    Remove UI setters and viewmodel observables for dashboard status

--- app/src/main/java/mattecarra/accapp/fragments/DashboardFragment.kt
+++ app/src/main/java/mattecarra/accapp/fragments/DashboardFragment.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.NumberPicker
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import com.afollestad.materialdialogs.MaterialDialog
 import com.afollestad.materialdialogs.customview.customView
@@ -24,6 +25,7 @@ import mattecarra.accapp.Preferences
 import mattecarra.accapp.R
 import mattecarra.accapp.acc.Acc
 import mattecarra.accapp.SharedViewModel
+import mattecarra.accapp.databinding.DashboardFragmentBinding
 import mattecarra.accapp.models.BatteryInfo
 import mattecarra.accapp.utils.ScopedFragment
 import java.util.concurrent.atomic.AtomicBoolean
@@ -42,7 +44,7 @@ class DashboardFragment : ScopedFragment() {
         fun newInstance() = DashboardFragment()
     }
 
-    private lateinit var viewModel: DashboardViewModel
+    private lateinit var mViewModel: DashboardViewModel
     private lateinit var configViewModel: SharedViewModel
     private lateinit var preferences: Preferences
 
@@ -50,7 +52,21 @@ class DashboardFragment : ScopedFragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.dashboard_fragment, container, false)
+
+        val binding: DashboardFragmentBinding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.dashboard_fragment,
+            container,
+            false
+        )
+
+        val view = binding.root
+
+        mViewModel = ViewModelProviders.of(this).get(DashboardViewModel::class.java)
+        binding.viewModel = mViewModel
+        binding.lifecycleOwner = this
+
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -60,16 +76,6 @@ class DashboardFragment : ScopedFragment() {
             preferences = Preferences(it)
 
             configViewModel = ViewModelProviders.of(it).get(SharedViewModel::class.java)
-
-            viewModel = ViewModelProviders.of(it).get(DashboardViewModel::class.java)
-
-            viewModel.getBatteryInfo().observe(this, Observer<BatteryInfo>{info ->
-                updateBatteryInfo(info)
-            })
-
-            viewModel.getIsDaemonRunning().observe(this, Observer<Boolean>{ daemon ->
-                updateAccdStatus(daemon)
-            })
 
             configViewModel.observeConfig(this, Observer { config ->
                 view.dash_resetStatusUnplug_switch.isChecked = config.configResetUnplugged
@@ -132,7 +138,7 @@ class DashboardFragment : ScopedFragment() {
                     }
                 }
 
-                viewModel.getIsDaemonRunning().observe(this@DashboardFragment, observer)
+                mViewModel.daemon.observe(this@DashboardFragment, observer)
 
                 withContext(Dispatchers.IO) {
                     if (stopDaemon)
@@ -143,7 +149,7 @@ class DashboardFragment : ScopedFragment() {
 
                 delay(5000)
 
-                viewModel.getIsDaemonRunning().removeObserver(observer)
+                mViewModel.daemon.removeObserver(observer)
 
                 if(!finished.getAndSet(true)) {
                     dash_daemonToggle_button.isEnabled = true
@@ -177,58 +183,25 @@ class DashboardFragment : ScopedFragment() {
     }
 
     /**
-     *  Update's UI components for the ACCD Status Card
-     */
-    private fun updateAccdStatus(isDaemonRunning: Boolean) {
-        if (isDaemonRunning) {
-            // ACCD Status Card
-            dash_accdStatus_textView.text = getString(R.string.acc_daemon_status_running)
-            dash_accdStatus_frameLay.background = ColorDrawable(resources.getColor(R.color.colorSuccessful))
-            dash_accdStatus_imageView.setImageResource(R.drawable.ic_baseline_check_circle_24px)
-
-            dash_daemonToggle_button.text = getString(R.string.stop)
-            dash_daemonToggle_button.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_outline_stop_24px, 0, 0, 0)
-        } else {
-            // ACCD Status Card
-            dash_accdStatus_textView.text = getString(R.string.acc_daemon_status_not_running)
-            dash_accdStatus_frameLay.background = ColorDrawable(resources.getColor(R.color.colorError))
-            dash_accdStatus_imageView.setImageResource(R.drawable.ic_baseline_error_24px)
-
-            dash_daemonToggle_button.text = getString(R.string.start)
-            dash_daemonToggle_button.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_outline_play_arrow_24px, 0, 0, 0)
-        }
-    }
-
-    /**
      * Function for ACCD status card OnClick in DashboardFragment
      */
     fun accdOnClick(view: View) {
         if (dash_accdButtons_linLay.visibility == View.GONE) {
             dash_accdButtons_linLay.visibility = View.VISIBLE
-            dash_title_accdStatus_textView.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_baseline_arrow_drop_up_24px, 0)
+            dash_title_accdStatus_textView.setCompoundDrawablesWithIntrinsicBounds(
+                0,
+                0,
+                R.drawable.ic_baseline_arrow_drop_up_24px,
+                0
+            )
         } else {
             dash_accdButtons_linLay.visibility = View.GONE
-            dash_title_accdStatus_textView.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_baseline_arrow_drop_down_24px, 0)
+            dash_title_accdStatus_textView.setCompoundDrawablesWithIntrinsicBounds(
+                0,
+                0,
+                R.drawable.ic_baseline_arrow_drop_down_24px,
+                0
+            )
         }
     }
-
-    /**
-     * Function for setting the respective battery text into their textviews.
-     * TODO: See if the performance is still a little jank, otherwise, use the handler to update UI elements within the observable.
-     */
-    private fun updateBatteryInfo(batteryInfo: BatteryInfo) {
-        // Battery Capacity
-        dash_batteryCapacity_pBar.progress = batteryInfo.capacity
-        // Battery Status (Charging (Fast)
-        dash_batteryStatus_textView.text = getString(R.string.info_status_extended, batteryInfo.status, batteryInfo.chargeType)
-        // Battery Speed (500mA at 4.11V)
-        val charging = batteryInfo.isCharging()
-        dash_batteryChargingSpeed_textView.text = if(charging) getString(R.string.info_charging_speed) else getString(R.string.info_discharging_speed)
-        dash_chargingSpeed_textView.text = getString(if(charging) R.string.info_charging_speed_extended else R.string.info_discharging_speed_extended, batteryInfo.getCurrentNow(preferences.uACurrent) * (if(charging) 1 else -1), batteryInfo.getVoltageNow(preferences.uVMeasureUnit))
-        // Battery Temperature
-        dash_batteryTemperature_textView.text = batteryInfo.temperature.toString().plus(Typography.degree)
-        // Battery Health
-        dash_batteryHealth_textView.text = batteryInfo.health
-    }
-
 }
