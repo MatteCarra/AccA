11066911f70b87860fef562c4a47ebd3543b20fa
Squabbi <squabbi@users.noreply.github.com>
2019-08-17 17:40:45 +1000

    Change to AndroidViewModel to use Preferences
    This allows the use of databinding whist being able to customise the output based on preferences.

--- app/src/main/java/mattecarra/accapp/fragments/DashboardViewModel.kt
+++ app/src/main/java/mattecarra/accapp/fragments/DashboardViewModel.kt
@@ -1,43 +1,53 @@
 package mattecarra.accapp.fragments
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel;
-import androidx.lifecycle.viewModelScope
+import android.app.Application
+import androidx.lifecycle.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import mattecarra.accapp.Preferences
 import mattecarra.accapp.acc.Acc
 import mattecarra.accapp.models.BatteryInfo
+import mattecarra.accapp.R
 
-class DashboardViewModel : ViewModel() {
-    private val batteryInfo: MutableLiveData<BatteryInfo> = MutableLiveData()
-    private val isDaemonRunning: MutableLiveData<Boolean> = MutableLiveData()
+class DashboardViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val _battery: MutableLiveData<BatteryInfo> = MutableLiveData()
+    private val _daemon: MutableLiveData<Boolean> = MutableLiveData(false)
+    private val _chargeSpeed: MutableLiveData<String> = MutableLiveData("N/A")
+
+    val battery: LiveData<BatteryInfo> = _battery
+    val daemon: LiveData<Boolean> = _daemon
+
+    private val mPreferences: Preferences = Preferences(application)
+
+    val chargeSpeed: LiveData<String> = _chargeSpeed
 
     @Volatile private var run = false
 
     init {
         viewModelScope.launch {
+
             while (true) {
-                if (batteryInfo.hasActiveObservers()) {
-                    batteryInfo.value = Acc.instance.getBatteryInfo()
+                if (battery.hasActiveObservers()) {
+                    val batteryInfo = Acc.instance.getBatteryInfo()
+                    _battery.value = withContext(Dispatchers.IO) {
+                        batteryInfo
+                    }
+
+                    _chargeSpeed.value = application.getString(if(batteryInfo.isCharging()) R.string.info_charging_speed_extended else R.string.info_discharging_speed_extended, batteryInfo.getCurrentNow(mPreferences.uACurrent) * (if(batteryInfo.isCharging()) 1 else -1), batteryInfo.getVoltageNow(mPreferences.uVMeasureUnit))
+
                 }
 
-                if(isDaemonRunning.hasActiveObservers()) {
-                    isDaemonRunning.value = Acc.instance.isAccdRunning()
+                if(daemon.hasActiveObservers()) {
+                    _daemon.value = withContext(Dispatchers.IO) {
+                        Acc.instance.isAccdRunning()
+                    }
                 }
 
                 delay(1000)
             }
         }
-    }
-
-    fun getBatteryInfo(): LiveData<BatteryInfo> {
-        return batteryInfo
-    }
-
-    fun getIsDaemonRunning(): LiveData<Boolean> {
-        return isDaemonRunning
     }
 }
