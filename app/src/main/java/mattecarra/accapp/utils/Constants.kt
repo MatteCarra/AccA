2470c5e2d056ebad4b093ce41a934236a3b4939e
Squabbi <squabbi@users.noreply.github.com>
2019-08-27 15:14:12 +1000

    Add schedule keys

--- app/src/main/java/mattecarra/accapp/utils/Constants.kt
+++ app/src/main/java/mattecarra/accapp/utils/Constants.kt
@@ -1,12 +1,21 @@
 package mattecarra.accapp.utils
 
 object Constants {
-    val PROFILE_KEY = "PROFILE"
+    val PROFILE_KEY = "profile"
     val PROFILE_ID_KEY = "profileId"
     val ACC_HAS_CHANGES = "hasChanges"
     val ACC_CONFIG_KEY = "accConfig"
-    val TITLE_KEY = "titleTv"
+    val TITLE_KEY = "title"
     val DATA_KEY = "data"
+
+    // CONFIG KEYS
+    val SCHEDULE_ID_KEY = "schedId"
+    val SCHEDULE_NAAME_KEY = "schedName"
+    val SCHEDULE_TIME_KEY = "schedTime"
+    val SCHEDULE_EXEC_ONCE_KEY = "schedExecOnce"
+    val SCHEDULE_EXEC_ONBOOT_KEY = "schedExecOnBoot"
+    val SCHEDULE_ENABLED_KEY = "schedEnabled"
+    val SCHEDULE_PROFILE_ID_KEY = "schedProfileId"
 
     //    PREFERENCE KEYS
     val CURRENT_UNIT_OF_MEASURE = "current_measure_unit"
