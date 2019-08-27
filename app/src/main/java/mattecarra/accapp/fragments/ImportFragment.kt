bda6662e83817c5bdf5e8619d99f831db71e4180
Squabbi <squabbi@users.noreply.github.com>
2019-08-27 16:42:43 +1000

    Create Import fragment class, viewmodel and layout

---
+++ app/src/main/java/mattecarra/accapp/fragments/ImportFragment.kt
@@ -0,0 +1,12 @@
+package mattecarra.accapp.fragments
+
+import mattecarra.accapp.utils.ScopedFragment
+
+class ImportFragment: ScopedFragment() {
+
+    companion object {
+        fun newInstance() = ImportFragment()
+    }
+
+
+}
