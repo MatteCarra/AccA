99705f74885d4fd2765a1aeae6e8cb2817cd941c
Squabbi <squabbi@users.noreply.github.com>
2019-09-18 09:00:38 +1000

    Add fragment page adapter

--- app/src/main/java/mattecarra/accapp/adapters/ImportExportFragmentPageAdapter.kt
+++ app/src/main/java/mattecarra/accapp/adapters/ImportExportFragmentPageAdapter.kt
@@ -2,24 +2,28 @@ package mattecarra.accapp.adapters
 
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentPagerAdapter
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import mattecarra.accapp.fragments.ExportFragment
 import mattecarra.accapp.fragments.ImportFragment
 
-class ImportExportFragmentPageAdapter internal constructor(fm: FragmentManager, behaviour: Int) :
-    FragmentPagerAdapter(fm, behaviour) {
+class ImportExportFragmentPageAdapter internal constructor(
+    fm: FragmentManager,
+    lifeCycle: Lifecycle
+) :
+    FragmentStateAdapter(fm, lifeCycle) {
 
     private val NUM_ITEMS = 2
 
-    override fun getItem(position: Int): Fragment {
+    override fun getItemCount(): Int {
+        return NUM_ITEMS
+    }
+
+    override fun createFragment(position: Int): Fragment {
         return when (position) {
             0 -> ExportFragment.newInstance()
             1 -> ImportFragment.newInstance()
             else -> ExportFragment.newInstance()
         }
-    }
-
-    override fun getCount(): Int {
-        return NUM_ITEMS
     }
 }
