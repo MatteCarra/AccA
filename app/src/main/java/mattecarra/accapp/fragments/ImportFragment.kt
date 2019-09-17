2abbf4969a42462e04e46319724d55fe8914cebc
Squabbi <squabbi@users.noreply.github.com>
2019-09-18 09:01:12 +1000

    Add inflater to ImportFragment

--- app/src/main/java/mattecarra/accapp/fragments/ImportFragment.kt
+++ app/src/main/java/mattecarra/accapp/fragments/ImportFragment.kt
@@ -1,5 +1,10 @@
 package mattecarra.accapp.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import mattecarra.accapp.R
 import mattecarra.accapp.utils.ScopedFragment
 
 class ImportFragment: ScopedFragment() {
@@ -8,5 +13,18 @@ class ImportFragment: ScopedFragment() {
         fun newInstance() = ImportFragment()
     }
 
+    private lateinit var mViewModel: ImportViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_import, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 
 }
