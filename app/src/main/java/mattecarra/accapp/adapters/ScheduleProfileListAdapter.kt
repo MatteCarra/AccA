359c14b98119125e5077a8baa40ebcaac00536e7
matteo <MatteCarra>
2019-09-27 20:57:03 +0200

    Added an expansion logic to schedules

--- app/src/main/java/mattecarra/accapp/adapters/ScheduleProfileListAdapter.kt
+++ app/src/main/java/mattecarra/accapp/adapters/ScheduleProfileListAdapter.kt
@@ -10,6 +10,8 @@ import android.widget.TextView
 import androidx.appcompat.widget.PopupMenu
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.switchmaterial.SwitchMaterial
+import kotlinx.android.synthetic.main.dashboard_fragment.*
+import kotlinx.android.synthetic.main.schedule_item.view.*
 import mattecarra.accapp.R
 import mattecarra.accapp.models.Schedule
 
@@ -26,21 +28,41 @@ class ScheduleProfileListAdapter internal constructor(context: Context) :
     private lateinit var mListener: OnScheduleClickListener
     private val mContext = context
 
-    inner class ProfileViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
-
-        override fun onClick(v: View?) {
-            mListener.onScheduleProfileClick(mScheduleList[adapterPosition])
-        }
-
-        init {
-            itemView.setOnClickListener(this)
-        }
+    inner class ProfileViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
         val titleTv: TextView = itemView.findViewById(R.id.item_schedule_title_tv)
         val profileTv: TextView = itemView.findViewById(R.id.item_schedule_profile_tv)
         val whenTv: TextView = itemView.findViewById(R.id.item_schedule_when_tv)
         val optionsIb: ImageButton = itemView.findViewById(R.id.item_schedule_options_ib)
         val scheduleToogleSwitch: SwitchMaterial = itemView.findViewById(R.id.item_schedule_toggle_switch)
+        val profileCapacityTv: TextView = itemView.findViewById(R.id.item_profile_capacity_tv)
+        val profileTemperatureTv: TextView = itemView.findViewById(R.id.item_profile_temperature_tv)
+        val profileOnPlugTv: TextView = itemView.findViewById(R.id.item_profile_on_plug_tv)
+
+
+        init {
+            itemView.setOnClickListener { v ->
+                if (v.schedule_details_ll.visibility == View.GONE) {
+                    v.schedule_details_ll.visibility = View.VISIBLE
+                    titleTv.setCompoundDrawablesWithIntrinsicBounds(
+                        0,
+                        0,
+                        R.drawable.ic_baseline_arrow_drop_up_24px,
+                        0
+                    )
+                } else {
+                    v.schedule_details_ll.visibility = View.GONE
+                    titleTv.setCompoundDrawablesWithIntrinsicBounds(
+                        0,
+                        0,
+                        R.drawable.ic_baseline_arrow_drop_down_24px,
+                        0
+                    )
+                }
+            }
+        }
+
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProfileViewHolder {
@@ -54,7 +76,7 @@ class ScheduleProfileListAdapter internal constructor(context: Context) :
 
         when {
             time != null && schedule.executeOnce -> {
-                holder.titleTv.text = "Exec. Once - " + time.hour + ":" + time.minute
+                holder.titleTv.text = mContext.getString(R.string.schedule_execute_once_row_title, time.hour, time.minute)
                 holder.whenTv.text = mContext.getString(R.string.schedule_execute_once_row_text, time.hour, time.minute)
             }
             time != null -> {
@@ -62,10 +84,15 @@ class ScheduleProfileListAdapter internal constructor(context: Context) :
                 holder.whenTv.text = mContext.getString(R.string.schedule_recurrent_row_text, time.hour, time.minute)
             }
             else -> {
-                holder.titleTv.text = "On Boot"
+                holder.titleTv.text = mContext.getString(R.string.execute_on_boot_schedule)
                 holder.whenTv.text = mContext.getString(R.string.schedule_boot_row_text)
             }
         }
+
+        val config = schedule.profile.accConfig
+        holder.profileCapacityTv.text = config.configCapacity.toString(mContext)
+        holder.profileTemperatureTv.text = config.configTemperature.toString(mContext)
+        holder.profileOnPlugTv.text = config.getOnPlug(mContext)
 
         holder.scheduleToogleSwitch.isChecked = schedule.isEnabled
         holder.scheduleToogleSwitch.setOnCheckedChangeListener { _, isChecked ->
@@ -80,6 +107,9 @@ class ScheduleProfileListAdapter internal constructor(context: Context) :
 
                 setOnMenuItemClickListener {
                     when (it.itemId) {
+                        R.id.schedules_option_menu_edit -> {
+                            mListener.onScheduleProfileClick(mScheduleList[position])
+                        }
                         R.id.schedules_option_menu_delete -> {
                             mListener.onScheduleDeleteClick(mScheduleList[position])
                         }
