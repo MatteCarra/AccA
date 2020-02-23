36476f477dfb63bffccee3b13c8773b63e73d2e0
MatteCarra <matte.carra@gmail.com>
2020-02-23 23:47:23 +0100

    Migration hot fix for people coming from older versions

--- app/src/main/java/mattecarra/accapp/database/AccaRoomDatabase.kt
+++ app/src/main/java/mattecarra/accapp/database/AccaRoomDatabase.kt
@@ -58,6 +58,8 @@ abstract class AccaRoomDatabase : RoomDatabase() {
 
         private val MIGRATION_6_7: Migration = object : Migration(6, 7) {
             override fun migrate(database: SupportSQLiteDatabase) {
+                database.execSQL("ALTER TABLE schedules_table ADD COLUMN `configResetBsOnPause` INTEGER NOT NULL DEFAULT 0");
+
                 database.execSQL("ALTER TABLE profiles_table ADD COLUMN `configCurrMax` INTEGER DEFAULT NULL");
                 database.execSQL("ALTER TABLE schedules_table ADD COLUMN `configCurrMax` INTEGER DEFAULT NULL");
             }
