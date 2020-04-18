0ba37293c681d442d9dd7228ba62adcb1f40ebd9
Squabbi <squabbi@users.noreply.github.com>
2020-04-18 17:34:19 +1000

    Expose fragment instances

--- app/src/main/java/mattecarra/accapp/adapters/ImportExportFragmentPageAdapter.kt
+++ app/src/main/java/mattecarra/accapp/adapters/ImportExportFragmentPageAdapter.kt
@@ -5,10 +5,14 @@ import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import mattecarra.accapp.fragments.ExportFragment
 import mattecarra.accapp.fragments.ImportFragment
+import kotlin.concurrent.fixedRateTimer
 
 private const val NUM_ITEMS = 2
 
 class ImportExportFragmentPageAdapter(fragment: FragmentActivity) : FragmentStateAdapter(fragment) {
+
+    private lateinit var importFragment: ImportFragment
+    private lateinit var exportFragment: ExportFragment
 
     override fun getItemCount(): Int {
         return NUM_ITEMS
@@ -16,9 +20,21 @@ class ImportExportFragmentPageAdapter(fragment: FragmentActivity) : FragmentStat
 
     override fun createFragment(position: Int): Fragment {
         return when (position) {
-            0 -> ImportFragment.newInstance()
-            1 -> ExportFragment.newInstance()
-            else -> ExportFragment.newInstance()
+            0 -> {
+                importFragment = ImportFragment.newInstance()
+                importFragment
+            }
+            1 -> {
+                exportFragment = ExportFragment.newInstance()
+                exportFragment
+            }
+            else -> {
+                exportFragment = ExportFragment.newInstance()
+                exportFragment
+            }
         }
     }
+
+    fun getImportFragment(): ImportFragment { return importFragment }
+    fun getExportFragment(): ExportFragment { return exportFragment }
 }
