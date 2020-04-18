728e9aee02c41f74954514a6e2d49523ec1ddacf
Squabbi <squabbi@users.noreply.github.com>
2020-04-18 17:31:30 +1000

    Remove unused imports

--- app/src/main/java/mattecarra/accapp/fragments/ExportFragment.kt
+++ app/src/main/java/mattecarra/accapp/fragments/ExportFragment.kt
@@ -1,23 +1,16 @@
 package mattecarra.accapp.fragments
 
-import android.content.Context
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.CompoundButton
-import android.widget.ListView
-import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.beust.klaxon.Klaxon
 import kotlinx.android.synthetic.main.fragment_export.*
 import mattecarra.accapp.R
 import mattecarra.accapp.adapters.ProfileExportAdapter
-import mattecarra.accapp.models.AccaProfile
 import mattecarra.accapp.models.ProfileExportItem
 import mattecarra.accapp.utils.ScopedFragment
 
