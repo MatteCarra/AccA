ef01892ade860268decfcee72b0bd53cff545207
Squabbi <squabbi@noreply.github.com>
2020-04-18 15:18:19 +1000

    Enable multidex

--- app/src/main/java/mattecarra/accapp/MainApplication.kt
+++ app/src/main/java/mattecarra/accapp/MainApplication.kt
@@ -2,9 +2,10 @@ package mattecarra.accapp
 
 import android.app.Application
 import android.content.Context
+import androidx.multidex.MultiDexApplication
 import com.topjohnwu.superuser.Shell
 
-class MainApplication: Application() {
+class MainApplication: MultiDexApplication() {
     companion object {
         init {
             Shell.Config.setFlags(Shell.FLAG_REDIRECT_STDERR)
