d088ba9bee2bd520d408b0ee463c2fc6f8ce9e5f
MatteCarra <matte.carra@gmail.com>
2021-07-26 10:24:13 +0200

    Update bundled acc version

--- app/src/main/java/mattecarra/accapp/acc/Acc.kt
+++ app/src/main/java/mattecarra/accapp/acc/Acc.kt
@@ -17,7 +17,7 @@ import java.net.URL
 import kotlin.math.abs
 
 object Acc {
-    const val bundledVersion = 202010150
+    const val bundledVersion = 202107240
     private val FILES_DIR = "/data/data/mattecarra.accapp/files"
 
     /*
