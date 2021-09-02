7e6e9321bfbbdb6e8f5170a524d9496a219e0da2
nialone <k85poc@yandex.ru>
2021-09-02 19:31:06 +0300

    reBind and optimize sheduleDialogExt

--- app/src/main/java/mattecarra/accapp/dialogs/ScheduleDialogExt.kt
+++ app/src/main/java/mattecarra/accapp/dialogs/ScheduleDialogExt.kt
@@ -14,13 +14,17 @@ import com.afollestad.materialdialogs.customview.getCustomView
 import kotlinx.coroutines.runBlocking
 import mattecarra.accapp.R
 import mattecarra.accapp.acc.Acc
+import mattecarra.accapp.databinding.ScheduleDialogBinding
+import mattecarra.accapp.databinding.VoltageControlEditorDialogBinding
 import mattecarra.accapp.models.AccaProfile
 import mattecarra.accapp.models.Schedule
 
-class ProfileSpinnerAdapter : BaseAdapter(), SpinnerAdapter {
+class ProfileSpinnerAdapter : BaseAdapter(), SpinnerAdapter
+{
     private var mList = emptyList<AccaProfile>()
 
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View
+    {
         val view =  convertView ?: LayoutInflater.from(parent.context).inflate(android.R.layout.simple_spinner_dropdown_item, parent, false)
         val textView = view.findViewById(android.R.id.text1) as TextView
         textView.text = getItem(position).profileName
@@ -28,90 +32,69 @@ class ProfileSpinnerAdapter : BaseAdapter(), SpinnerAdapter {
     }
 
     fun setItems(list: List<AccaProfile>) {
-        if(mList != list) {
-            mList = list
-        }
+        if(mList != list) mList = list
         notifyDataSetChanged()
     }
 
-    override fun getItem(position: Int): AccaProfile {
-        return mList[position]
-    }
+    override fun getItem(position: Int): AccaProfile = mList[position]
 
-    override fun getItemId(position: Int): Long {
-        return mList[position].uid.toLong()
-    }
+    override fun getItemId(position: Int): Long = mList[position].uid.toLong()
 
-    override fun getCount(): Int {
-        return mList.size
-    }
+    override fun getCount(): Int = mList.size
 
 }
 
-typealias AddScheduleListener =
-        ((profileId: Long, scheduleName: String, time: String, executeOnce: Boolean, executeOnBoot: Boolean) -> Unit)
-
+typealias AddScheduleListener = ((profileId: Long, scheduleName: String, time: String,
+                                  executeOnce: Boolean, executeOnBoot: Boolean) -> Unit)
 
 fun MaterialDialog.addScheduleDialog(
     profilesLiveData: LiveData<List<AccaProfile>>,
     profiles: MutableList<AccaProfile> = mutableListOf(AccaProfile(-1, context.getString(R.string.new_config), runBlocking { Acc.instance.readDefaultConfig() })),
     schedule: Schedule? = null,
     listener: AddScheduleListener
-): MaterialDialog {
+): MaterialDialog
+{
     val adapter = ProfileSpinnerAdapter()
     val observer = Observer<List<AccaProfile>> {
         profiles.addAll(it)
         adapter.setItems(profiles)
     }
 
-    val dialog = customView(R.layout.schedule_dialog)
-        .positiveButton(R.string.save) { dialog ->
-            val view = dialog.getCustomView()
-            val spinner = view.findViewById<Spinner>(R.id.profile_selector)
-            val timePicker = view.findViewById<TimePicker>(R.id.time_picker)
-            val scheduleType = view.findViewById<Spinner>(R.id.schedule_type_selector).selectedItemId
-            val scheduleName = view.findViewById<EditText>(R.id.schedule_name_edit_text)
-            val executeOnBootCheckBox = view.findViewById<CheckBox>(R.id.execute_on_boot_checkbox)
+    val binding = ScheduleDialogBinding.inflate(layoutInflater)
+    customView(view = binding.root)
 
-            val time = if(scheduleType == 2L) {
-                "boot"
-            } else {
-                val hour =
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) timePicker.hour else timePicker.currentHour
-                val minute =
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) timePicker.minute else timePicker.currentMinute
+    val spinner = binding.profileSelector
+    val timePicker = binding.timePicker
+    val timePickerContainer = binding.timePickerContainer
+    val scheduleTypeSpinner = binding.scheduleTypeSelector
+    val scheduleType = binding.scheduleTypeSelector.selectedItemId
+    val scheduleName = binding.scheduleNameEditText
+    val executeOnBootCheckBox = binding.executeOnBootCheckbox
 
+    positiveButton(R.string.save) { dialog ->
+
+            val time = if(scheduleType == 2L) "boot" else
+            {
+                val hour = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) timePicker.hour else timePicker.currentHour
+                val minute = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) timePicker.minute else timePicker.currentMinute
                 "${String.format("%02d", hour)}${String.format("%02d", minute)}"
             }
 
             listener(spinner.selectedItemId, scheduleName.text.toString(), time, scheduleType == 1L, executeOnBootCheckBox.isChecked && scheduleType != 2L)
         }
-        .onDismiss {
-            profilesLiveData.removeObserver(observer)
-        }
-
-
-    val customView = dialog.getCustomView()
-    val spinner = customView.findViewById<Spinner>(R.id.profile_selector)
-    val timePicker = view.findViewById<TimePicker>(R.id.time_picker)
-    val scheduleTypeSpinner = customView.findViewById<Spinner>(R.id.schedule_type_selector)
-    val scheduleNameEditText = view.findViewById<EditText>(R.id.schedule_name_edit_text)
-    val executeOnBootCheckBox = view.findViewById<CheckBox>(R.id.execute_on_boot_checkbox)
+        .onDismiss { profilesLiveData.removeObserver(observer) }
 
     profilesLiveData.observeForever(observer)
     spinner.adapter = adapter
 
-    scheduleTypeSpinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
+    scheduleTypeSpinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener
+    {
         override fun onNothingSelected(parent: AdapterView<*>?) {}
-
-        override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long)
+        {
             when(id) {
-                 2L -> {
-                     customView.findViewById<LinearLayout>(R.id.time_picker_container).visibility = View.GONE
-                 }
-                else -> {
-                    customView.findViewById<LinearLayout>(R.id.time_picker_container).visibility = View.VISIBLE
-                }
+                 2L -> timePickerContainer.visibility = View.GONE
+                else -> timePickerContainer.visibility = View.VISIBLE
             }
         }
     }
@@ -132,12 +115,11 @@ fun MaterialDialog.addScheduleDialog(
             timePicker.currentMinute = minute
         }
 
-        scheduleNameEditText.setText(schedule.profile.scheduleName)
-
+        scheduleName.setText(schedule.profile.scheduleName)
         executeOnBootCheckBox.isChecked = schedule.executeOnBoot
     }
 
-    return dialog
+    return this
 }
 
 typealias EditScheduleListener =
